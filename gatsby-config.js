ce36bc679d26e9865f79444f01235eb7eddc2be8
Surudh Bhutani <bsurudh@gmail.com>
2020-06-10 13:30:40 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `John Doe`,
+    author: `John Doe`,
+    description: `John Doe's personal site`,
+    keywords: [`John`, `Doe`, `Personal`, `Blog`, `Resume`, `Projects`, `Work`],
+    url: process.env.URL || process.env.DEPLOY_URL || ``,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `John Doe's Personal Site`,
+        short_name: `J.Doe`,
+        description: `This is my personal site.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#ggg`,
+        display: `standalone`,
+        icon: `${__dirname}/static/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
